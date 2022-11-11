4760862fbb4965034ea109497afebb780fdf7473
omxr4216 <117938635+omxr4216@users.noreply.github.com>
2022-11-11 18:18:10 +0300

    Update sfetch.cpp

--- sfetch.cpp
+++ sfetch.cpp
@@ -58,7 +58,7 @@ int main() {
             std::cout << "" << username;
             std::cout << "@" << u.nodename;
             std::cout << "\n";
-            std::cout << "OS: " << u.sysname;
+            std::cout << "OS: ";
             FILE *fp;
             char path[1035];
             fp = popen("/usr/bin/lsb_release --short -d", "r");
