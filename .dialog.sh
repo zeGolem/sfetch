f2e80843769eb66583dc167eae39ca3aae5932ef
omxr4216 <117938635+omxr4216@users.noreply.github.com>
2022-11-12 12:02:17 +0300

    Delete .dialog.sh

--- .dialog.sh
+++
@@ -1,9 +0,0 @@
-# This file is for the gui option
-# DO NOT DELETE THIS FILE
-
-if (whiptail --title "Install sfetch" --yesno "Would you like to install sfetch?" 8 78); then
-	make install
-	whiptail --title "Install sfetch" --msgbox "sfetch Successfully Installed!" 8 78
-    else
-        echo "exited"
-fi
